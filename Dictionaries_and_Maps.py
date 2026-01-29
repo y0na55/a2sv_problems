aab5c0178ba49648bb41934718bde4118f4cfa69
J0na555 <ethiopediaa@gmail.com>
2026-01-29 23:01:12 +0300

    solved one another problem

--- Dictionaries_and_Maps.py
+++ Dictionaries_and_Maps.py
@@ -7,8 +7,8 @@ for _ in range(n):
     name, number = input().split()
     phone_book[name] = number
     
-for line in sys.stdin:
-    query = line.strip()
+for name in sys.stdin:
+    query = name.strip()
     
     if query in phone_book:
         print(f"{query}={phone_book[query]}")
