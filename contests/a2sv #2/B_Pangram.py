ebd84ed641f1755cf47b7b7056c16477e492fa5e
J0na555 <ethiopediaa@gmail.com>
2026-02-02 23:20:03 +0300

    solved the pangram problem

--- contests/a2sv #2/B_Pangram.py
+++ contests/a2sv #2/B_Pangram.py
@@ -1,70 +1,12 @@
-import sys
-
-input = sys.stdin.readline
-
 n = int(input())
+word = input().lower()
 
-words = input().split()
+word_set = set(word)
 
-word = ''.join(words)
-count = 0
-
-
-# print(word)
-
-for char in word:
-    if char.isupper():
-        print("YES")
-        break
-    else:
-        print("NO")
-        break
-
-# if count:
-#     print("YES")
-
-# else:
-#     print("NO")
+if len(word_set) < 26:
+    print("NO")
+else:
+    print("YES")
 
 
 
-# if any(char.isupper() for char in words):
-#     print("YES")
-
-# print("NO")
-
-
-# any(char.isupper() for char in word)
-
-# if any(char.isupper() for char in words):
-#     print("YES")
-
-# print("NO")
-
-
-# if all(char.isupper() for char in words):
-#     print("YES")
-
-# print("NO")
-
-
-# count = 0
-
-# for char in words:
-#     if any(char.isupper()):
-#         count += 1
-#         break
-
-# if count:
-#     print("YES")
-
-# print("NO")
-
-
-
-# for char in words:
-#     if any(char.isupper()):
-#         print("Yes")
-#         break
-#     else:
-#         print("NO")
